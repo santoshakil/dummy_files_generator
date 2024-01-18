c23a57b4c420e2d537fdaaa03b481978969cf62c
Santo Shakil <shakil.mezbah@gmail.com>
2024-01-18 13:36:33 +0600

    fixes

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,11 @@ use std::io::BufWriter;
 use std::io::Write;
 use std::thread;
 
-const CHUNK_SIZE: usize = 1_048_576; // 1MB in bytes
+const CHUNK_SIZE: usize = 1_048_576;
 
-const LARGE_FILE_SIZE: usize = 307_152; // 500MB in KB
-const MEDIUM_FILE_SIZE: usize = 104_800; // 200MB in KB
-const SMALL_FILE_SIZE: usize = 1024; // 1MB in KB
+const LARGE_FILE_SIZE: usize = 307_152;
+const MEDIUM_FILE_SIZE: usize = 104_800;
+const SMALL_FILE_SIZE: usize = 1024;
 
 const LARGE_FILE_COUNT: usize = 20;
 const MEDIUM_FILE_COUNT: usize = 100;
@@ -25,9 +25,9 @@ fn create_file(file_path: String, file_size: usize) {
     let chunks = file_size / CHUNK_SIZE;
     let remainder = file_size % CHUNK_SIZE;
     for _ in 0..chunks {
-        writer.write_all(&vec![0; CHUNK_SIZE]).unwrap(); // Write 1MB of zeros
+        writer.write_all(&vec![0; CHUNK_SIZE]).unwrap();
     }
-    writer.write_all(&vec![0; remainder]).unwrap(); // Write the remaining bytes
+    writer.write_all(&vec![0; remainder]).unwrap();
     println!("File {} created.", file_path);
 }
 
@@ -43,8 +43,8 @@ fn main() {
     let small_files_thread = thread::spawn(move || {
         // Generate small files
         (0..SMALL_FILE_COUNT).into_par_iter().for_each(|i| {
-            let random_folder1: u32 = rand::thread_rng().gen_range(0..1000);
-            let random_folder2: u32 = rand::thread_rng().gen_range(0..1000);
+            let random_folder1: u32 = rand::thread_rng().gen_range(0..100000);
+            let random_folder2: u32 = rand::thread_rng().gen_range(0..100000);
             let dir = if i % 2 == 0 { "src" } else { "dst" };
             let file_path = format!(
                 "{}/{}/{}/{}/small_file{}.txt",
@@ -62,8 +62,8 @@ fn main() {
     let medium_files_thread = thread::spawn(move || {
         // Generate medium files
         (0..MEDIUM_FILE_COUNT).into_par_iter().for_each(|i| {
-            let random_folder1: u32 = rand::thread_rng().gen_range(0..1000);
-            let random_folder2: u32 = rand::thread_rng().gen_range(0..1000);
+            let random_folder1: u32 = rand::thread_rng().gen_range(0..100000);
+            let random_folder2: u32 = rand::thread_rng().gen_range(0..100000);
             let file_path = format!(
                 "{}/dst/{}/{}/medium_file{}.txt",
                 drive_location, random_folder1, random_folder2, i
@@ -80,8 +80,8 @@ fn main() {
     let large_files_thread = thread::spawn(move || {
         // Generate large files
         (0..LARGE_FILE_COUNT).into_par_iter().for_each(|i| {
-            let random_folder1: u32 = rand::thread_rng().gen_range(0..1000);
-            let random_folder2: u32 = rand::thread_rng().gen_range(0..1000);
+            let random_folder1: u32 = rand::thread_rng().gen_range(0..100000);
+            let random_folder2: u32 = rand::thread_rng().gen_range(0..100000);
             let file_path = format!(
                 "{}/src/{}/{}/large_file{}.txt",
                 drive_location, random_folder1, random_folder2, i
